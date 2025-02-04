39bdbbe9642d5ff3d7f623cbc8a3b146dce56d47
Abdurrahman Raza <abdurrahmanraza.ed@gmail.com>
2025-02-04 12:02:54 -0800

    server action to store to database

--- snippets/src/app/snippets/new/page.tsx
+++ snippets/src/app/snippets/new/page.tsx
@@ -1,8 +1,32 @@
-import React from "react";
+import { db } from "@/db";
+import { redirect } from "next/navigation";
 
 export default function SnippetCreatePage() {
+  async function createSnippet(formData: FormData) {
+    // this needs to be a server action, the line below is a next.js thing for how it determines server actions
+    "use server";
+
+    // check user's inputs and make sure they're valid
+    const title = formData.get("title") as string; // the data for this "title" is gathered from the <input> in the return with name="title"
+    const code = formData.get("code") as string; // same as above but instead with "code"
+
+    // create a new record in the database
+    const snippet = await db.snippet.create({
+      data: {
+        //title: title, | this can just be written as title because the const title above and the title created here have the same name
+        //code: code,
+        title,
+        code,
+      },
+    });
+    console.log(snippet);
+
+    // redirect to homepage (or to saved snippet page once we get to that)
+    redirect("/");
+  }
+
   return (
-    <form>
+    <form action={createSnippet}>
       <h3 className="font-bold m-3">Create a Snippet</h3>
       <div className="flex flex-col gap-4">
         <div className="flex gap-4">
--- snippets/src/db/index.ts
+++ snippets/src/db/index.ts
@@ -1,14 +1,3 @@
 import { PrismaClient } from "@prisma/client";
 // "handle" onto our database, used for save, delete, edit, etc.
 export const db = new PrismaClient();
-
-/*
-// example creating snippet
-
-db.snippet.create({
-    data:{
-        title: "Title!",
-        code: "const abc = () => {}"
-    }
-})
-*/
