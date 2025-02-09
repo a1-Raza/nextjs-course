c72961915d7651a2cddc7a13ab55da97b4e9a03d
Abdurrahman Raza <abdurrahmanraza.ed@gmail.com>
2025-02-08 19:34:37 -0800

    edit page, linking pages from home, etc.; also section 3 complete

--- snippets/src/app/snippets/[id]/page.tsx
+++ snippets/src/app/snippets/[id]/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { notFound } from "next/navigation";
 import { db } from "@/db";
 
@@ -8,6 +9,8 @@ interface SnippetShowPageProps {
 }
 
 export default async function SnippetShowPage(props: any) {
+  await new Promise((r) => setTimeout(r, 500)); // for artificial loading
+
   const { id } = await props.params;
 
   const snippet = await db.snippet.findFirst({
@@ -18,5 +21,23 @@ export default async function SnippetShowPage(props: any) {
     return notFound();
   }
 
-  return <div>{snippet.title}</div>;
+  return (
+    <div>
+      <div className="flex m-4 justify-between items-center">
+        <h1 className="text-xl font-bold">{snippet.title}</h1>
+        <div className="flex gap-4">
+          <Link
+            href={`/snippets/${snippet.id}/edit`}
+            className="p-2 border rounded"
+          >
+            Edit
+          </Link>
+          <button className="p-2 border rounded">Delete</button>
+        </div>
+      </div>
+      <pre className="p-3 border rounded bg-gray-200 border-gray-200">
+        <code>{snippet.code}</code>
+      </pre>
+    </div>
+  );
 }
